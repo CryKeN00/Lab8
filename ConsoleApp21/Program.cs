9e6b452b72ed015bb22d9e2139c35498e7c97f93
BondarcovNikolai <BondacovNikolaix@gmail.com>
2023-12-22 08:32:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите x и y");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y;
+
+    if (x == 0)
+    {
+        Console.WriteLine("Ошибка");
+    }
+    else
+    {
+        Console.WriteLine(z);
+    }
+
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Деление на ноль");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
